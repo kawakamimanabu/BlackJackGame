1bdc85855941502a2b9f9854ab939392b7fbe69d
Kawakami Manabu <kawakamimanabu@users.noreply.github.com>
2015-04-13 15:50:47 +0900

    Update BjPlayerTest.java

--- src/com/freshpeople/training/blackjack/test/BjPlayerTest.java
+++ src/com/freshpeople/training/blackjack/test/BjPlayerTest.java
@@ -27,37 +27,101 @@ public class BjPlayerTest {
 		assertEquals(bjPlayer.getPlayerName(), "test");
 	}
 
+	/**
+	 * addCard と getSum のテスト
+	 */
 	@Test
-	public void testAddCard() {
+	public void testAddCardGetSum() {
 		bjPlayer.addCard(new Card("Club",1));
 		assertEquals(bjPlayer.getSum(), 11);
 		bjPlayer.addCard(new Card("Club",11));
 		assertEquals(bjPlayer.getSum(), 21);
 	}
 
+	/**
+	 * clearCard のテスト
+	 */
 	@Test
 	public void testClearCard() {
 		bjPlayer.addCard(new Card("Club",1));
-		bjPlayer.addCard(new Card("Club",11));
-		assertEquals(bjPlayer.getSum(), 21);
+		assertEquals(bjPlayer.getSum(), 11);
 		bjPlayer.clearCard();
 		assertEquals(bjPlayer.getSum(), 0);
 	}
 
+	/**
+	 * bust 判定のテスト1
+	 * カードが無い場合に Bust していないこと
+	 */
 	@Test
-	public void testBust() {
-		bjPlayer.addCard(new Card("Club",9));
-		bjPlayer.addCard(new Card("Club",11));
+	public void testBust_1() {
+		assertFalse(bjPlayer.isBust());
+	}
+
+	/**
+	 * bust 判定のテスト2
+	 * カードの合計が21の場合に Bust していないこと
+	 */
+	@Test
+	public void testBust_2() {
+		bjPlayer.addCard(new Card("Club",8));
+		bjPlayer.addCard(new Card("Club",3));
 		bjPlayer.addCard(new Card("Club",12));
+		assertFalse(bjPlayer.isBust());
+	}
+
+	/**
+	 * bust 判定のテスト3
+	 * カードの合計が21の場合に Bust していること
+	 */
+	@Test
+	public void testBust_3() {
+		bjPlayer.addCard(new Card("Club",10));
+		bjPlayer.addCard(new Card("Club",11));
+		bjPlayer.addCard(new Card("Club",2));
 		assertTrue(bjPlayer.isBust());
 	}
 
 	/**
-	 * 3 枚すべて A の時の合計テスト
+	 * 合計値は 20 となり、Diff が 1 となること
+	 */
+	@Test
+	public void testGetDiff_1() {
+		bjPlayer.addCard(new Card("Club",8));
+		bjPlayer.addCard(new Card("Heart",2));
+		bjPlayer.addCard(new Card("Spade",12));
+		assertEquals(bjPlayer.getSum(), 20);
+		assertEquals(bjPlayer.getDiff(), 1);
+	}
+
+	/**
+	 * 合計値は 21 となり、Diff が 0 となること
+	 */
+	@Test
+	public void testGetDiff_2() {
+		bjPlayer.addCard(new Card("Club",8));
+		bjPlayer.addCard(new Card("Heart",3));
+		bjPlayer.addCard(new Card("Spade",12));
+		assertEquals(bjPlayer.getDiff(), 0);
+	}
+
+
+	/**
+	 * 合計値は 22 となり、Diff が -1 となること
+	 */
+	@Test
+	public void testGetDiff_3() {
+		bjPlayer.addCard(new Card("Club",2));
+		bjPlayer.addCard(new Card("Heart",13));
+		bjPlayer.addCard(new Card("Spade",12));
+		assertEquals(bjPlayer.getDiff(), -1);
+	}
+
+	/**
 	 * 合計値は 23 となり、Diff が -2 となること
 	 */
 	@Test
-	public void testGetSumWhen3Ace() {
+	public void testGetDiff_3A() {
 		bjPlayer.addCard(new Card("Club",1));
 		bjPlayer.addCard(new Card("Heart",1));
 		bjPlayer.addCard(new Card("Spade",1));
