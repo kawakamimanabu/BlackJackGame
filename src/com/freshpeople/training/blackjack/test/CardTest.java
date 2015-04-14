41f7af02e81ef3470efce9a28e575f9d2315ea32
kawakamimanabu <mnvkawa@gmail.com>
2015-04-14 18:10:10 +0900

    updated

--- src/com/freshpeople/training/blackjack/test/CardTest.java
+++ src/com/freshpeople/training/blackjack/test/CardTest.java
@@ -18,12 +18,15 @@ public class CardTest {
 
 	@Test
 	public void testGetStringNumber() {
+		// A の文字を取得できること
 		Card c = new Card("Club", 1);
 		assertEquals(c.getStringNumber(), "A");
+		// 2 から 10 の文字が取得できること
 		for (int i = 2; i <= 10; i++) {
 			c = new Card("Club", i);
 			assertEquals(c.getStringNumber(), String.valueOf(i));
 		}
+		// J,Q,K の文字が取得できること
 		c = new Card("Club", 11);
 		assertEquals(c.getStringNumber(), "J");
 		c = new Card("Club", 12);
