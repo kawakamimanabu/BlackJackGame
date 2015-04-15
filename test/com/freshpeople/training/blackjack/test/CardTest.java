cbe73d9b5c5860959b56459f6e20c85e97aba7ae
Kawakami Manabu <kawakamimanabu@users.noreply.github.com>
2015-04-15 10:59:01 +0900

    中身の処理を抜きました。

--- test/com/freshpeople/training/blackjack/test/CardTest.java
+++ test/com/freshpeople/training/blackjack/test/CardTest.java
@@ -10,28 +10,12 @@ public class CardTest {
 
 	@Test
 	public void testGetIntNumber() {
-		for (int i = 1; i <= 13; i++) {
-			Card c = new Card("Club", i);
-			assertEquals(c.getIntNumber(), i);
-		}
 	}
 
 	@Test
 	public void testGetStringNumber() {
 		// A の文字を取得できること
-		Card c = new Card("Club", 1);
-		assertEquals(c.getStringNumber(), "A");
 		// 2 から 10 の文字が取得できること
-		for (int i = 2; i <= 10; i++) {
-			c = new Card("Club", i);
-			assertEquals(c.getStringNumber(), String.valueOf(i));
-		}
 		// J,Q,K の文字が取得できること
-		c = new Card("Club", 11);
-		assertEquals(c.getStringNumber(), "J");
-		c = new Card("Club", 12);
-		assertEquals(c.getStringNumber(), "Q");
-		c = new Card("Club", 13);
-		assertEquals(c.getStringNumber(), "K");
 	}
 }
