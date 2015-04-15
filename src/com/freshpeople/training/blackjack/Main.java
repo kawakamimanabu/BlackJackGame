771cbf73f2fc38081f11e6e46171f5f7c6a64e74
Kawakami Manabu <kawakamimanabu@users.noreply.github.com>
2015-04-15 11:09:23 +0900

    中身の処理を抜きました。

--- src/com/freshpeople/training/blackjack/Main.java
+++ src/com/freshpeople/training/blackjack/Main.java
@@ -1,16 +1,9 @@
 package com.freshpeople.training.blackjack;
 
+/**
+ * ゲームを行うメインクラス
+ */
 public class Main {
-
 	public static void main(String[] args) {
-		BjDealer com = new BjDealer("dealer");
-		BaseBjPlayer you = new BjPlayer("you");
-		try {
-			com.addBjPlayer(you);
-			com.startGame();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
 	}
-
 }
