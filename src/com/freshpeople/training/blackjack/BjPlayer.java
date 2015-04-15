90ead33b3b960c5321e348dad6d14759e8bccf0d
Kawakami Manabu <kawakamimanabu@users.noreply.github.com>
2015-04-15 11:06:53 +0900

    中身の処理を抜きました。

--- src/com/freshpeople/training/blackjack/BjPlayer.java
+++ src/com/freshpeople/training/blackjack/BjPlayer.java
@@ -13,38 +13,9 @@ import java.io.InputStreamReader;
 public class BjPlayer extends BaseBjPlayer {
 
 	public BjPlayer(String playerName) {
-		super(playerName);
 	}
 
 	@Override
 	public boolean judgeHit() {
-		showMyCards();
-		if (isBust()) {
-			return false;
-		}
-		int i = 0;
-		for(;;) {
-			System.out.println("Input number [1] to add more card, else input [0]");
-			try
-			{
-				InputStreamReader reader = new InputStreamReader(System.in);
-				BufferedReader br = new BufferedReader(reader);
-				String val = br.readLine().trim();
-				i = Integer.parseInt(val);
-				if (i != 1 && i != 0) {
-					System.out.println("wrong number!! please input [0] or [1].");
-				}
-				else {
-					if (i == 1) return true;
-					else return false;
-				}
-			}
-			catch (Exception ex) {
-				System.out.println(ex.getMessage());
-				break;
-			}
-		}
-		return false;
 	}
-
 }
