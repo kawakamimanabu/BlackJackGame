4f13df314f29daec2c0d645275ed2e0ec9a739f1
Kawakami Manabu <kawakamimanabu@users.noreply.github.com>
2015-04-15 11:06:00 +0900

    中身の処理を抜きました。

--- src/com/freshpeople/training/blackjack/BjDealer.java
+++ src/com/freshpeople/training/blackjack/BjDealer.java
@@ -14,9 +14,9 @@ import java.util.stream.Collectors;
 public class BjDealer extends BaseBjPlayer implements DealerTask {
 	private CardManagerTask cardManagerTask = new CardManager();
 	private List<BaseBjPlayer> bjPlayerList = new ArrayList<>();
-	private List<BaseBjPlayer> winList		= new ArrayList<>();
-	private List<BaseBjPlayer> loseList		= new ArrayList<>();
-	private List<BaseBjPlayer> drawList		= new ArrayList<>();
+	private List<BaseBjPlayer> winList	= new ArrayList<>();
+	private List<BaseBjPlayer> loseList	= new ArrayList<>();
+	private List<BaseBjPlayer> drawList	= new ArrayList<>();
 
 	// この数以下の場合は必ずヒットする
 	private int point = 16;
@@ -26,62 +26,33 @@ public class BjDealer extends BaseBjPlayer implements DealerTask {
 	 * @param playerName
 	 */
 	public BjDealer(String playerName) {
-		super(playerName);
 	}
 
 	@Override
 	public boolean judgeHit() {
-		if (getSum() <= point)
-			return true;
-		return false;
 	}
 
 	@Override
 	public void showMyFirstCard() {
-		if (myCardList.size() > 1) {
-			System.out.println("\t*** dealer's first card ***");
-			System.out.println("\t" + myCardList.get(0).toString());
-			System.out.println("\t********");
-		}
 	}
 
 	@Override
 	public void addBjPlayer(BaseBjPlayer bjPlayer) throws Exception{
-		if (!bjPlayerList.contains(bjPlayer)) {
-			bjPlayerList.add(bjPlayer);
-		}
-		else {
-			throw new Exception("Duplicate BlackJack Player Name.[" + bjPlayer.getPlayerName() + "]");
-		}
 	}
 
 	@Override
 	public List<String> getPlayerNameList() {
-		Function<BaseBjPlayer, String> mapper = bjPlayer -> bjPlayer.getPlayerName();
-		return bjPlayerList.stream().map(mapper).collect(Collectors.toList());
 	}
 
 	@Override
 	public void startGame() throws Exception {
 		// 勝敗者リストを初期化
-		winList.clear();
-		loseList.clear();
-		drawList.clear();
 		// カードを初期化
-		cardManagerTask.init();
 		// ディーラーを含む全プレイヤーにカードを配る
-		dealCards();
 		// ディーラー自身の最初の 1 枚を公開する
-		showMyFirstCard();
 		// ディーラー以外の Player が Hit する場合にカードを配る
-		for (BaseBjPlayer player : bjPlayerList) {
-			// Player が Hit しなくなるまでカードを配る
-			askHitDealCard(player);
-		}
 		// 最後にディーラー自身に対して Hit する
-		askHitDealCard(this);
 		// 結果判定
-		judgeGame();
 	}
 
 	//--- private methods ---
@@ -91,13 +62,7 @@ public class BjDealer extends BaseBjPlayer implements DealerTask {
 	 */
 	private void dealCards() throws Exception {
 		// 全 Player に 2 枚ずつカードを配る
-		for (BaseBjPlayer player : bjPlayerList) {
-			player.addCard(cardManagerTask.getRandomCard());
-			player.addCard(cardManagerTask.getRandomCard());
-		}
 		// ディーラー自身にカードを配る
-		addCard(cardManagerTask.getRandomCard());
-		addCard(cardManagerTask.getRandomCard());
 	}
 
 	/**
@@ -107,33 +72,12 @@ public class BjDealer extends BaseBjPlayer implements DealerTask {
 	 * @throws Exception
 	 */
 	private void askHitDealCard(BaseBjPlayer player) throws Exception {
-		while (player.judgeHit()) {
-			player.addCard(cardManagerTask.getRandomCard());
-		}
 	}
 
 	/**
 	 * 結果判定
 	 */
 	private void judgeGame() {
-		System.out.println("\n\n>>>>>>>>>> Game Result <<<<<<<<<<");
-		showMyCards();
-		for(BaseBjPlayer player : bjPlayerList) {
-			player.showMyCards();
-			if (player.isBust() && isBust() || player.getDiff() == getDiff()) {
-				System.out.println("Draw.");
-				drawList.add(player);
-			}
-			else if (player.isBust() && !isBust() || (player.getDiff() > getDiff() && !isBust())) {
-				System.out.println(player.getPlayerName() + " missed the game...");
-				loseList.add(player);
-			}
-			else {
-				System.out.println(player.getPlayerName() + " won the game!!");
-				winList.add(player);
-			}
-		}
-		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 	}
 
 
@@ -143,27 +87,23 @@ public class BjDealer extends BaseBjPlayer implements DealerTask {
 	 * @param cardManagerTask
 	 */
 	public void setCardManagerTask(CardManagerTask cardManagerTask) {
-		this.cardManagerTask = cardManagerTask;
 	}
 
 	/**
 	 * @return winList
 	 */
 	public List<BaseBjPlayer> getWinList() {
-		return winList;
 	}
 
 	/**
 	 * @return loseList
 	 */
 	public List<BaseBjPlayer> getLoseList() {
-		return loseList;
 	}
 
 	/**
 	 * @return drawList
 	 */
 	public List<BaseBjPlayer> getDrawList() {
-		return drawList;
 	}
 }
